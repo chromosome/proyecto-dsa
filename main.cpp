200d0b8c04a83d14d8d7eb352e91c4cfc570d4a7
Danilo Bonometti <danilobonometti@udec.cl>
2020-08-19 19:03:39 -0400

    agregada prueba de 'remove'

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char const *argv[])
 	dsa::quad_tree qtree(read_data(filename));
 	cout << "inserted: " << qtree.size() << endl << endl;
 
-	// busqueda
+	// busqueda ---------------------------------------------------------------
 	point_t city = {50,29};
 	// point_t city = {50.523429, 29.461285};
 	data_t* d = qtree.find(city);
@@ -26,10 +26,10 @@ int main(int argc, char const *argv[])
 	else
 		cout << "not found!" << endl << endl;
 
-	// profundidad de un punto
+	// profundidad de un punto ------------------------------------------------
 	cout << "depth of " << city << ": " << qtree.depth(city) << endl << endl;
 
-	// conteo
+	// conteo -----------------------------------------------------------------
 	long int total_population = 0;
 	quad_t q = {{40, 40}, {2, 2}};
 	// quad_t q = {{0, 0}, {90, 180}};
@@ -37,6 +37,14 @@ int main(int argc, char const *argv[])
 	qtree.region_search(q);
 
 	cout << "total population: " << total_population << endl;
+
+	// remove -----------------------------------------------------------------
+	data_t* city2 = new data_t({{25, 39}, 1240810});
+	if (qtree.insert(city2))
+		cout << "inserted:" << city2->first << endl;
+
+	if (qtree.remove(city2))
+		cout << "removed:" << city2->first << endl;
 
 	return 0;
 }
