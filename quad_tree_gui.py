8ba529232ef0c44334819083f8ee8d5ad397f972
Danilo Bonometti <danilobonometti@udec.cl>
2020-08-26 01:50:29 -0400

    pruebas de gui

--- quad_tree_gui.py
+++ quad_tree_gui.py
@@ -1,7 +1,7 @@
 import sys, pygame
 from pygame.locals import*
 
-#import quad_tree
+import quad_tree
 
 ###################################################################
 
@@ -15,71 +15,86 @@ Color_line   = (255,0,0)
 Color_circle = (0,255,0)
 circle_radius = 5
 
-screen = pygame.display.set_mode((Screen_width,Screen_height))
+screen = pygame.display.set_mode((Screen_width, Screen_height))
 pygame.display.set_caption('Deku Tree')
 
 screen.fill(Color_screen)
 pygame.display.flip()
 
-#tree = quad_tree.quadtree(Screen_width,Screen_height)
+tree = quad_tree.quad_tree(Screen_width, Screen_height)
 points = []
 
 ###################################################################
 
 def draw_subdivision_driver(tracks, x, y, w, h):
 
-    pygame.draw.line(screen,Color_line,(x + w/2,y),(x+w/2,y+h))
-    pygame.draw.line(screen,Color_line,(x,y+h/2),(x+w,y+h/2))
+	# pygame.draw.line(screen,Color_line,(x + w/2,y),(x+w/2,y+h))
+	# pygame.draw.line(screen,Color_line,(x,y+h/2),(x+w,y+h/2))
 
-    for track in tracks:
+	for track in tracks:
 
-        w = w/2
-        h = h/2
+		if track == "origin":
+			pass
+		elif(track == "NW"):
+			w = w/2
+			h = h/2
+			x = x
+			y = y
+			pygame.draw.line(screen,Color_line,(x + w/2,y),(x+w/2,y+h))
+			pygame.draw.line(screen,Color_line,(x,y+h/2),(x+w,y+h/2))
+		elif(track == "NE"):
+			w = w/2
+			h = h/2
+			x = x+w
+			y = y
+			pygame.draw.line(screen,Color_line,(x + w/2,y),(x+w/2,y+h))
+			pygame.draw.line(screen,Color_line,(x,y+h/2),(x+w,y+h/2))
+		elif(track == "SW"):
+			w = w/2
+			h = h/2
+			x = x
+			y = y+h
+			pygame.draw.line(screen,Color_line,(x + w/2,y),(x+w/2,y+h))
+			pygame.draw.line(screen,Color_line,(x,y+h/2),(x+w,y+h/2))
+			print(x,y,w,h)
+		elif(track == "SE"):
+			w = w/2
+			h = h/2
+			x = x+w
+			y = y+h
+			pygame.draw.line(screen,Color_line,(x + w/2,y),(x+w/2,y+h))
+			pygame.draw.line(screen,Color_line,(x,y+h/2),(x+w,y+h/2))
 
-        if(track == "NW"):
-            x = x
-            y = y
-            pygame.draw.line(screen,Color_line,(x + w/2,y),(x+w/2,y+h))
-            pygame.draw.line(screen,Color_line,(x,y+h/2),(x+w,y+h/2))
-        elif(track == "NE"):
-            x = x+w
-            y = y
-            pygame.draw.line(screen,Color_line,(x + w/2,y),(x+w/2,y+h))
-            pygame.draw.line(screen,Color_line,(x,y+h/2),(x+w,y+h/2))
-        elif(track == "SW"):
-            x = x
-            y = y+h
-            pygame.draw.line(screen,Color_line,(x + w/2,y),(x+w/2,y+h))
-            pygame.draw.line(screen,Color_line,(x,y+h/2),(x+w,y+h/2))
-            print(x,y,w,h)
-        elif(track == "SE"):
-            x = x+w
-            y = y+h
-            pygame.draw.line(screen,Color_line,(x + w/2,y),(x+w/2,y+h))
-            pygame.draw.line(screen,Color_line,(x,y+h/2),(x+w,y+h/2))
-
-        pygame.display.flip()
+		pygame.display.flip()
 
 
 def draw_subdivision(tracks):
-    draw_subdivision_driver(tracks, 0, 0, Screen_width, Screen_height)
+	draw_subdivision_driver(tracks, 0, 0, Screen_width, Screen_height)
 
 
 def insert(x,y):
-    p = Point()
-    p.center = (x,y)
-    p.value = 0
-    points.append(p)
-    p.draw()
+	p = Point()
+	p.center = (x,y)
+	p.value = 0
+	points.append(p)
+	p.draw()
 
 
 def clear_screen():
-    screen.fill(Color_screen)
-    pygame.display.flip()
+	screen.fill(Color_screen)
+	pygame.display.flip()
 
 
 def mouseClicked(pos):
-    insert(pos[0], pos[1])
+	print(pos)
+	insert(pos[0], pos[1])
+	x = pos[0] - Screen_width/2
+	y = pos[1] - Screen_height/2
+	print(x, y)
+	tree.insert(x, y)
+	tracks = tree.track(x, y)
+	print(tracks)
+	draw_subdivision(tracks)
 
 
 
@@ -87,27 +102,24 @@ def mouseClicked(pos):
 
 class Point():
 
-    center = (0,0)
-    value  = 0
+	center = (0,0)
+	value  = 0
 
-    def draw(self):
-        pygame.draw.circle(screen, Color_circle, self.center, circle_radius)
-        pygame.display.flip()
+	def draw(self):
+		pygame.draw.circle(screen, Color_circle, self.center, circle_radius)
+		pygame.display.flip()
 
 ###################################################################
 
 def main():
+	while True:
+		for events in pygame.event.get():
+			if events.type == QUIT:
+				sys.exit(0)
 
-    draw_subdivision(["SE","NW","NW"])
-
-    while True:
-        for events in pygame.event.get():
-            if events.type == QUIT:
-                sys.exit(0)
-
-            if events.type == pygame.MOUSEBUTTONDOWN:
-                  pos = pygame.mouse.get_pos()
-                  mouseClicked(pos)
+			if events.type == pygame.MOUSEBUTTONDOWN:
+				  pos = pygame.mouse.get_pos()
+				  mouseClicked(pos)
 
 
 main()
